6192b4c598bf09ea0e682b4b5f771e66c3abb1d6
ikennaumeh <kennaumeh@gmail.com>
2023-11-08 18:16:33 +0100

    Written deploy script

--- script/DeployBoardman.s.sol
+++ script/DeployBoardman.s.sol
@@ -5,5 +5,10 @@ import {Script} from "forge-std/Script.sol";
 import {Boardman} from "../src/Boardman.sol";
 
 contract DeployBoardman is Script {
-    function run() external returns()
+    function run() external returns(Boardman){
+        vm.startBroadcast();
+        Boardman boardman = new Boardman();
+        vm.stopBroadcast();
+        return boardman;
+    }
 }
